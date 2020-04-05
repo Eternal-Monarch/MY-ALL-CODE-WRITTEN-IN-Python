e741a26297a2a43c4a3199ef209e509ab4527ca1
Subhasish Dutta <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2020-04-05 07:38:54 +0600

    Create program 2.py

---
+++ program 2.py
@@ -0,0 +1,14 @@
+name=input("Enter ur name :")#for user inputed
+age=input("enter ur age : ")# use it "#" for adding string
+department=input("enter ur department : ")
+
+
+
+
+
+
+print("student information")
+print("_________")
+print("Name : "+name)
+print("Age : "+age)
+print("Brainware University : "+department)
