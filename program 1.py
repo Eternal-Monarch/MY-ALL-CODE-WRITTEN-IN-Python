72520a5bc33e504d5c20f3784634f56f56c66492
Subhasish Dutta <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2020-04-05 07:23:50 +0600

    Create program 1.py

---
+++ program 1.py
@@ -0,0 +1,7 @@
+name="shuvo"
+print(3+4)#no semicolon needed
+print(5-4)#here use # for write in comment
+print(5*3)
+print(25//5)#here we "//"sign for donot showing the 5.0
+print(350%2)
+print("we use ",name,"for u")#here two string together using and adding for another string we use ,name,
